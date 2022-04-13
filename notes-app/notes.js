4eee918fc5aa6f4b4b881af3eace87eca4198beb
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-14 00:40:29 +0530

    feat: create a remove note method

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,24 +1,8 @@
 const fs = require("fs");
+const chalk = require("chalk");
 
 const getNotes = function () {
   return "Your notes...";
-};
-
-const addNote = (title, body) => {
-  const notes = loadNotes();
-  const newNote = { title, body };
-
-  const doesNoteAlreadyExist = notes.find(
-    (note) => note.title === newNote.title
-  );
-
-  if (!doesNoteAlreadyExist) {
-    notes.push(newNote);
-    saveNotes(notes);
-    console.log("New note added!");
-  } else {
-    console.log("Note title taken!");
-  }
 };
 
 const loadNotes = () => {
@@ -37,4 +21,33 @@ const saveNotes = (notes) => {
   fs.writeFileSync("notes.json", dataJSON);
 };
 
-module.exports = { getNotes, addNote };
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const newNote = { title, body };
+
+  const doesNoteAlreadyExist = notes.find(
+    (note) => note.title === newNote.title
+  );
+
+  if (!doesNoteAlreadyExist) {
+    notes.push(newNote);
+    saveNotes(notes);
+    console.log(chalk.green.inverse("New note added!"));
+  } else {
+    console.log(chalk.red.inverse("Note title taken!"));
+  }
+};
+
+const removeNote = (title) => {
+  const currentNotes = loadNotes();
+  const updatedNotes = currentNotes.filter((note) => note.title !== title);
+
+  if (currentNotes.length === updatedNotes.length) {
+    console.log(chalk.red.inverse("No such note!"));
+  } else {
+    saveNotes(updatedNotes);
+    console.log(chalk.green.inverse("Note removed!"));
+  }
+};
+
+module.exports = { getNotes, addNote, removeNote };
