4e52ad534249cfb4336403df8fb5cc51fccaab81
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-14 23:44:03 +0530

    feat: print current temperature and feels like

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,6 +3,11 @@ require("dotenv").config();
 
 const URL = `http://api.weatherstack.com/current?access_key=${process.env.API_ACCESS_KEY}&query=New York`;
 
-request({ url: URL }, (error, response) => {
-  
+request({ url: URL, json: true }, (error, response) => {
+  const data = response.body;
+  const currentTemperatureData = data.current;
+
+  console.log(
+    `It is currently ${currentTemperatureData.temperature} degrees out. It feels like ${currentTemperatureData.feelslike} degrees out.`
+  );
 });
