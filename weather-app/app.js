1f6ab989347fa7a7b2fb8f92d6b23b07f0b376d2
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-15 11:53:19 +0530

    feat: implement geocoding for address

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,23 @@
 const request = require("request");
 require("dotenv").config();
 
-const URL = `http://api.weatherstack.com/current?access_key=${process.env.API_ACCESS_KEY}&query=New York`;
+const WEATHER_URL = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_ACCESS_KEY}&query=New York&units=f`;
+const GEOCODING_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.GEOCODING_API_ACCESS_KEY}`;
 
-request({ url: URL, json: true }, (error, response) => {
+request({ url: WEATHER_URL, json: true }, (error, response) => {
   const data = response.body;
   const currentTemperatureData = data.current;
 
   console.log(
-    `It is currently ${currentTemperatureData.temperature} degrees out. It feels like ${currentTemperatureData.feelslike} degrees out.`
+    `${currentTemperatureData.weather_descriptions[0]}. It is currently ${currentTemperatureData.temperature} degrees out. It feels like ${currentTemperatureData.feelslike} degrees out.`
   );
+});
+
+// Geocoding: address -> lat/long
+request({ url: GEOCODING_URL, json: true }, (error, response) => {
+  const data = response.body;
+  const longitude = data.features[0].center[0];
+  const latitude = data.features[0].center[1];
+
+  console.log(latitude, longitude);
 });
