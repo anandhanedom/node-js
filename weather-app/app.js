83a8b104df8da6f94d87c7b4a6c7cc28107b8e49
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-16 10:58:29 +0530

    feat: implement error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,19 +5,31 @@ const WEATHER_URL = `http://api.weatherstack.com/current?access_key=${process.en
 const GEOCODING_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.GEOCODING_API_ACCESS_KEY}`;
 
 request({ url: WEATHER_URL, json: true }, (error, response) => {
-  const data = response.body;
-  const currentTemperatureData = data.current;
+  if (error) {
+    console.log("Unable to connect to weather services!");
+  } else if (response.body.error) {
+    console.log("Unable to find location!");
+  } else {
+    const data = response.body;
+    const currentTemperatureData = data.current;
 
-  console.log(
-    `${currentTemperatureData.weather_descriptions[0]}. It is currently ${currentTemperatureData.temperature} degrees out. It feels like ${currentTemperatureData.feelslike} degrees out.`
-  );
+    console.log(
+      `${currentTemperatureData.weather_descriptions[0]}. It is currently ${currentTemperatureData.temperature} degrees out. It feels like ${currentTemperatureData.feelslike} degrees out.`
+    );
+  }
 });
 
 // Geocoding: address -> lat/long
 request({ url: GEOCODING_URL, json: true }, (error, response) => {
-  const data = response.body;
-  const longitude = data.features[0].center[0];
-  const latitude = data.features[0].center[1];
+  if (error) {
+    console.log("Unable to connect to location services!");
+  } else if (!response.body.features.length) {
+    console.log("Unable to find location! Try another search.");
+  } else {
+    const data = response.body;
+    const longitude = data.features[0].center[0];
+    const latitude = data.features[0].center[1];
 
-  console.log(latitude, longitude);
+    console.log(latitude, longitude);
+  }
 });
