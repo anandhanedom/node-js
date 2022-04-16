ae14961d2ac70bd27502fab62b7512706bc9eb76
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-16 21:57:52 +0530

    fix: serve full public dir

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,15 +2,11 @@ const path = require("path");
 
 const express = require("express");
 
-const homePagePath = path.join(__dirname, "../public");
-const helpPagePath = path.join(__dirname, "../public/help.html");
-const aboutPagePath = path.join(__dirname, "../public/about.html");
+const publicDirPath = path.join(__dirname, "../public");
 
 const app = express();
 
-app.use(express.static(homePagePath));
-app.use("/about", express.static(aboutPagePath));
-app.use("/help", express.static(helpPagePath));
+app.use(express.static(publicDirPath));
 
 app.get("/weather", (req, res) => {
   res.send({ forecast: "Partly cloudy", location: "Wayanad" });
