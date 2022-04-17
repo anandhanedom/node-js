16e51ac31d78f2769c4863fc5f3aa59189d209ae
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-17 23:00:33 +0530

    feat: partials and views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,20 @@
 const path = require("path");
-
 const express = require("express");
-
-const publicDirPath = path.join(__dirname, "../public");
+const hbs = require("hbs");
 
 const app = express();
 
+// Paths for express config
+const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirPath));
 
 // Home page
@@ -28,7 +36,7 @@ app.get("/about", (req, res) => {
 //Help page
 app.get("/help", (req, res) => {
   res.render("help", {
-    title: "How can I help you?",
+    title: "Help",
     name: "John Doe",
   });
 });
