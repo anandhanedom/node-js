fd682d2e107c2bf2594d6250e42286f9615f1c66
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-17 12:47:11 +0530

    feat: render dynamic pages using handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,7 +6,32 @@ const publicDirPath = path.join(__dirname, "../public");
 
 const app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirPath));
+
+// Home page
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "John Doe",
+  });
+});
+
+//About page
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "John Doe",
+  });
+});
+
+//Help page
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "How can I help you?",
+    name: "John Doe",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({ forecast: "Partly cloudy", location: "Wayanad" });
