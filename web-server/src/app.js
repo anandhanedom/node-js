51cf44ff11d76e1a710acc7797a9108971eeb7c5
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-17 23:19:09 +0530

    feat: 404 view

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -45,6 +45,22 @@ app.get("/weather", (req, res) => {
   res.send({ forecast: "Partly cloudy", location: "Wayanad" });
 });
 
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "John Doe",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "John Doe",
+    errorMessage: "Page not found",
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
 });
