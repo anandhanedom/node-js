4f4e62a8065698c9c684554fe3dd9832e830ffac
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-18 19:06:00 +0530

    feat: implement search input

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,27 @@
 console.log("Client-side JS is loaded!");
 
-fetch("http://localhost:3000/weather?address=wayanad")
-  .then((res) => res.json())
-  .then((data) => {
-    if (data.error) {
-      return console.log(data.error);
-    }
+const fetchWeatherDetails = (location) => {
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        return console.log(data.error);
+      }
 
-    console.log(data.location);
-    console.log(data.forecast);
-  })
-  .catch((err) => console.log(err));
+      console.log(data.location);
+      console.log(data.forecast);
+    })
+    .catch((err) => console.log(err));
+};
+
+const formElement = document.querySelector("form");
+const inputElement = document.querySelector("input");
+
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = inputElement.value;
+  if (location) {
+    fetchWeatherDetails(location);
+  }
+});
