4e48dcaaea970bb82657a7470ab9c15a49397a2b
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-18 21:52:17 +0530

    style: add more styles for home page

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,21 +1,36 @@
 console.log("Client-side JS is loaded!");
 
+const showDomOutput = (isLoading, error, data) => {
+  if (isLoading) {
+    messageOneElement.textContent = "Loading...";
+    messageTwoElement.textContent = "";
+  } else if (error) {
+    messageOneElement.textContent = error;
+    messageTwoElement.textContent = "";
+  } else {
+    messageOneElement.textContent = data.location;
+    messageTwoElement.textContent = data.forecast;
+  }
+};
+
 const fetchWeatherDetails = (location) => {
+  showDomOutput(true);
   fetch(`http://localhost:3000/weather?address=${location}`)
     .then((res) => res.json())
     .then((data) => {
       if (data.error) {
-        return console.log(data.error);
+        return showDomOutput(false, data.error, null);
       }
 
-      console.log(data.location);
-      console.log(data.forecast);
+      showDomOutput(false, null, data);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => showDomOutput(false, err));
 };
 
 const formElement = document.querySelector("form");
 const inputElement = document.querySelector("input");
+const messageOneElement = document.querySelector("#messageOne");
+const messageTwoElement = document.querySelector("#messageTwo");
 
 formElement.addEventListener("submit", (e) => {
   e.preventDefault();
