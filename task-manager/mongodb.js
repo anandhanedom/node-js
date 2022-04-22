b5ad72c57fdd8ccf9ea4b5e76045cc2c36b2a48d
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-04-23 00:04:21 +0530

    feat: insertOne and insertMany

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -16,9 +16,47 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "John Doe",
-      age: 24,
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "John Doe",
+    //     age: 24,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user!");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection("users").insertMany(
+      [
+        { name: "Jane Doe", age: 22 },
+        { name: "Jack Doe", age: 26 },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert users!");
+        }
+
+        console.log(result.ops);
+      }
+    );
+
+    db.collection("tasks").insertMany(
+      [
+        { description: "Learn node", completed: false },
+        { description: "Pot plants", completed: true },
+        { description: "Watch football", completed: false },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert tasks!");
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
